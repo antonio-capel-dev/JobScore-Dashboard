be066141f19bd94dfcd7561ec92d815643adac54
AntonioCapelRamirez1991 <antoniomiguelcapelramirez@gmail.com>
2026-08-05 19:01:58 +0200

    feat: da estilo con Tailwind al listado de ofertas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,21 @@ import { useOffers } from "./hooks/useOffers";
 function App() {
   const ofertas = useOffers();
   return (
-    <div>
-    <h1 className="text-3xl font-bold text-blue-600">JobScore Dashboard</h1>
-    {ofertas.map((oferta) => {
-      return <p key={oferta.id}> {oferta.titulo_puesto} - {oferta.score}</p>
+    <div className="p-8 max-w-2xl mx-auto">
+    <h1 className="text-3xl font-bold text-blue-600 mb-6">JobScore Dashboard</h1>
+    <div className="space-y-3">
+
+     {ofertas.map((oferta) => {
+        return <div key={oferta.id} className="bg-amber-200 rounded-lg shadow p-4 flex justify-between items-center border">
+                
+        <h2 className="font-semibold text-lg">{oferta.titulo_puesto}
+        </h2>
+        <p className="text-gray-500 text-sm">{oferta.empresa}</p>
+        <span className="px-3 py-1 rounded-full text-white font-bold bg-amber-500">{oferta.score}</span>
+        </div>
     })}
+    </div>
+
     </div>
   )
 }
